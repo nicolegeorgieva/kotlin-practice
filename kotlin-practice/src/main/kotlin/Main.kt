5b010dc0132be559a24f32aef5d726c61c5556ce
Nicole <nicole.georgievva@gmail.com>
2023-01-17 16:13:52 +0200

    exercise string functions

--- kotlin-practice/src/main/kotlin/Main.kt
+++ kotlin-practice/src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var myCar = "My car won't start"
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("\"${myCar.substring(3, 6)}\"")
 }
