5d75188202016f35282829340c0761c99417e9ca
Nicole <nicole.georgievva@gmail.com>
2023-01-17 18:25:34 +0200

    Exercise number input and print result type

---
+++ kotlin-practice/src/main/kotlin/NumberInputAndTypes.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Write a number")
+
+    val userInput = readlnOrNull() ?: ""
+    val userInputToInt = userInput.toIntOrNull()
+    val doubleTypeNum = 5.42
+
+    val result = if (userInputToInt != null) userInputToInt * doubleTypeNum else "Invalid number"
+
+    println(result::class.java)
+}
