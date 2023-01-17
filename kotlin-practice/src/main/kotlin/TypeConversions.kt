b3947aca705074a193330a792b93b99fcec10863
Nicole <nicole.georgievva@gmail.com>
2023-01-17 21:02:10 +0200

    Exercise type conversions

--- kotlin-practice/src/main/kotlin/TypeConversions.kt
+++ kotlin-practice/src/main/kotlin/TypeConversions.kt
@@ -1,7 +1,18 @@
 fun main() {
     val pi = 3.14159
 
-    println(pi.toFloat())
-    println(pi.toInt())
-    println(pi.toString())
+//    println(pi.toFloat())
+//    println(pi.toInt())
+//    println(pi.toString())
+
+    println("Enter a number of type double")
+
+    val userInput = readlnOrNull()
+    val userInputToDouble = userInput?.toDoubleOrNull()
+
+    if (userInputToDouble != null) {
+        println("$userInput is of type ${userInputToDouble::class.java}")
+    } else {
+        println("Invalid type")
+    }
 }
