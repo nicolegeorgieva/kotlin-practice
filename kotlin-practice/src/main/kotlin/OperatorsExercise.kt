1bfcbb8f3e9690678d256dfad8d4b51bc1edab1e
Nicole <nicole.georgievva@gmail.com>
2023-01-18 14:19:20 +0200

    Exercise operators

--- kotlin-practice/src/main/kotlin/OperatorsExercise.kt
+++ kotlin-practice/src/main/kotlin/OperatorsExercise.kt
@@ -1,14 +1,23 @@
 fun main() {
-    println("Write the amount you have in your bank account.")
+    println("Write the interest for your bank account.")
 
-    println(calculateMoney())
+    val interest = readlnOrNull()?.toDoubleOrNull()
+
+    if (interest != null) {
+        val totalSumAfterAYear = 1 + interest / 100
+        println(calculateMoney(totalSumAfterAYear))
+    } else {
+        println("Invalid interest.")
+    }
 }
 
-private fun calculateMoney(): String {
+private fun calculateMoney(interest: Double): String {
+    println("Write your bank amount.")
+
     val bankAmount = readlnOrNull()?.toDoubleOrNull() ?: return "Invalid amount"
     if (bankAmount < 0) return "Amount must be a positive number"
     if (bankAmount > 10000) return "We don't support interests after 10 000"
 
-    val newBankAmount = bankAmount * 1.055 * 1.055 * 1.055 * 1.055 * 1.055
+    val newBankAmount = bankAmount * interest * interest * interest * interest * interest
     return "Your final amount will be $newBankAmount"
 }
