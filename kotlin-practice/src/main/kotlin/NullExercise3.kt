dbdee670951038b5ce3f01162f88b21c7596d1d4
Nicole <nicole.georgievva@gmail.com>
2023-01-18 18:00:59 +0200

    Null exercise 3

---
+++ kotlin-practice/src/main/kotlin/NullExercise3.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("Please insert a number")
+    val number = readlnOrNull()
+
+    val result = number?.toDoubleOrNull()?.times(7)?.toString()
+    println("Length of $result is ${result?.length}")
+}
