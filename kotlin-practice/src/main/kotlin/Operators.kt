bf9049cce63146fada6d04e5116b8ee818b3b834
Nicole <nicole.georgievva@gmail.com>
2023-01-18 13:29:21 +0200

    Shorter solution of the exercise

--- kotlin-practice/src/main/kotlin/Operators.kt
+++ kotlin-practice/src/main/kotlin/Operators.kt
@@ -1,13 +1,24 @@
+import java.text.DecimalFormat
+
 fun main(args: Array<String>) {
+//    println("Write your bank account amount")
+//
+//    val userBankAccountAmount = readlnOrNull()
+//    val contestPrize = userBankAccountAmount?.toDoubleOrNull()
+//
+//    if (contestPrize != null) {
+//        val reward = contestPrize * 3
+//        println("You have a total of ${reward}.")
+//    } else {
+//        println("Invalid amount.")
+//    }
+
     println("Write your bank account amount")
 
-    val userBankAccountAmount = readlnOrNull()
-    val contestPrize = userBankAccountAmount?.toDoubleOrNull()
+    val result = readlnOrNull()?.toDoubleOrNull()?.times(3)?.let { result ->
+        val formatted = DecimalFormat("###,###.##").format(result)
+        "The result is ${formatted}"
+    } ?: "Invalid amount"
 
-    if (contestPrize != null) {
-        val reward = contestPrize * 3
-        println("You have a total of ${reward}.")
-    } else {
-        println("Invalid amount.")
-    }
+    println(result)
 }
