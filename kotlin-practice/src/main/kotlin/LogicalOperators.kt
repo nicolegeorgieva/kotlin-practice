aec87704e6fd6d98fb53f201560d14e55e24dd66
Nicole <nicole.georgievva@gmail.com>
2023-01-18 16:54:08 +0200

    Exercise logical operators

---
+++ kotlin-practice/src/main/kotlin/LogicalOperators.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val toys = 2
+    val brokenToys = 1
+    val startToys = toys - brokenToys
+    val oneNotBroken = startToys >= 1
+
+    val newToy = toys < 5 && oneNotBroken
+    println(newToy)
+}
