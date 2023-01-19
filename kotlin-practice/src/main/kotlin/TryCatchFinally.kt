22aeb086af90275111e58e9f5a26415ed919425b
Nicole <nicole.georgievva@gmail.com>
2023-01-19 15:43:40 +0200

    Try Catch Finally exercise

---
+++ kotlin-practice/src/main/kotlin/TryCatchFinally.kt
@@ -0,0 +1,18 @@
+fun main() {
+    try {
+        println("What product would you like to buy?")
+        val product = readln()
+        if (product.isBlank()) error("Product is blank.")
+
+        println("How many of it do you require?")
+        val quantity = readln().toInt()
+        val productPrice = 9.99
+
+        println("Total sum: ${quantity.times(productPrice)}")
+    } catch (e: Exception) {
+        e.printStackTrace()
+        println("An exception has occurred \n ${e.localizedMessage}")
+    } finally {
+        println("The execution has completed")
+    }
+}
