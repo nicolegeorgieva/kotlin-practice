b01a4f19f3d592088b12222f4b2c9692bb26d7a5
Nicole <nicole.georgievva@gmail.com>
2023-01-19 15:03:23 +0200

    Improve exercise solution

--- kotlin-practice/src/main/kotlin/TryCatch.kt
+++ kotlin-practice/src/main/kotlin/TryCatch.kt
@@ -4,6 +4,8 @@ fun main() {
         println(test?.toInt())
     } catch (e: Exception) {
         e.printStackTrace()
-        println("An exception happened \n ${e.localizedMessage}")
+        println("An exception has occurred \n ${e.localizedMessage}")
+    } finally {
+        println("The execution has completed")
     }
 }
