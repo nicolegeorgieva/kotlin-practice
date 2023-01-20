b6c33e8bc5a2facc25a9ac9227b11644789319b6
Nicole <nicole.georgievva@gmail.com>
2023-01-20 19:22:38 +0200

    Set functions exercise 3

---
+++ kotlin-practice/src/main/kotlin/SetFunctionsExercise3.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var acceptedColors = hashSetOf("white", "black", "grey")
+    val myColors = setOf("blue", "red", "black", "green")
+
+    acceptedColors.retainAll(myColors)
+    println(acceptedColors)
+
+    acceptedColors = hashSetOf("white", "black", "grey")
+    acceptedColors.add("red")
+    acceptedColors.retainAll(myColors)
+    println(acceptedColors)
+}
