02db75b71126bab56d2afee29e9cfb1f2ba585ce
Nicole <nicole.georgievva@gmail.com>
2023-01-20 15:35:49 +0200

    List and array list exercise 3

---
+++ kotlin-practice/src/main/kotlin/ListAndArrayListExercise3.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val items = arrayListOf("laptop", "mouse", "pen", "paper", "mug", "phone")
+    val itemsToRemove = listOf("pen", "paper", "mug", "phone")
+
+    items.removeAll(itemsToRemove)
+    println(items)
+}
