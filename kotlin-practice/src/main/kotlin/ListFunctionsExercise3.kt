4b556424eb28ac6428c0d52d804862b3bfd26c8f
Nicole <nicole.georgievva@gmail.com>
2023-01-20 16:15:52 +0200

    List functions exercise 3

---
+++ kotlin-practice/src/main/kotlin/ListFunctionsExercise3.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val animals = arrayListOf("lion", "zebra", "chimp", "elephant")
+
+    animals.add("panda")
+    println(animals)
+
+    animals.remove("lion")
+    println(animals)
+
+    val condition = listOf("elephant", "giraffe")
+    println(animals.containsAll(condition))
+}
