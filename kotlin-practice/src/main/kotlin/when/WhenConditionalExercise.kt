c69797ab1585df9eeffdeefcf9589af4e2dd2962
Nicole <nicole.georgievva@gmail.com>
2023-01-23 15:38:19 +0200

    Refactor when conditional exercise

--- kotlin-practice/src/main/kotlin/when/WhenConditionalExercise.kt
+++ kotlin-practice/src/main/kotlin/when/WhenConditionalExercise.kt
@@ -12,8 +12,7 @@ fun main() {
     }
 
     when (meals) {
-        0 -> println("You should eat.")
-        in 1..2 -> println("You should eat at least $recommendedAmount meals a day.")
+        in 0..2 -> println("You should eat at least $recommendedAmount meals a day.")
         3 -> println("Great, you eat the recommended amount of $recommendedAmount meals a day.")
         else -> println("You should eat less. The recommended amount is $recommendedAmount meals a day.")
     }
