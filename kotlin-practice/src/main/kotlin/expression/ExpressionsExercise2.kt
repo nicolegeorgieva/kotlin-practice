2327ab9264046929bfc9baf2ac02f91832f520a1
Nicole <nicole.georgievva@gmail.com>
2023-01-23 13:33:59 +0200

    Solve expressions exercise 2

---
+++ kotlin-practice/src/main/kotlin/expression/ExpressionsExercise2.kt
@@ -0,0 +1,20 @@
+package expression
+
+fun main() {
+    println("How many cats do you have?")
+    val input = readlnOrNull()
+    val catsCount = input?.toIntOrNull()
+
+    if (catsCount == null) {
+        println("Invalid input")
+        return
+    }
+
+    if (catsCount in 1..3) {
+        println("few cats")
+    } else if (catsCount in 4..6) {
+        println("several cats")
+    } else {
+        println("many cats")
+    }
+}
