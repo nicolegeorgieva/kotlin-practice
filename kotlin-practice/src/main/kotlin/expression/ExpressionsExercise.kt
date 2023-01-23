639624f52b363848bb181f54d0cd2df44d093d8f
Nicole <nicole.georgievva@gmail.com>
2023-01-23 13:20:38 +0200

    Solve expressions exercise

---
+++ kotlin-practice/src/main/kotlin/expression/ExpressionsExercise.kt
@@ -0,0 +1,14 @@
+package expression
+
+fun main() {
+    val guestList = setOf("John", "Alice", "Bob", "Carol")
+    println("Please enter your name")
+
+    val guest = readlnOrNull()?.takeIf { it.isNotBlank() }
+    if (guest == null) {
+        println("Invalid input")
+        return
+    }
+
+    if (guest in guestList) println("Hi, $guest, please come in.") else println("Your name doesn't exist in the guest list.")
+}
