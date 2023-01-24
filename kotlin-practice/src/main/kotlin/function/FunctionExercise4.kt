809c87db8351a8c71f93f75a1a230366649c2d28
Nicole <nicole.georgievva@gmail.com>
2023-01-24 20:43:32 +0200

    Solve function exercise 4

---
+++ kotlin-practice/src/main/kotlin/function/FunctionExercise4.kt
@@ -0,0 +1,9 @@
+package function
+
+fun main() {
+    println(sumIntegers(5, 6))
+    println(sumIntegers(7, 1))
+    println(sumIntegers(9, 1))
+}
+
+fun sumIntegers(number1: Int, number2: Int) = number1 + number2
