89453858b985716dcabc8a630d697d3d4e6bb7cb
Nicole <nicole.georgievva@gmail.com>
2023-01-24 21:29:27 +0200

    Refactor function exercise 5

--- kotlin-practice/src/main/kotlin/function/FunctionExercise5.kt
+++ kotlin-practice/src/main/kotlin/function/FunctionExercise5.kt
@@ -3,10 +3,15 @@ package function
 fun main() {
     val products = hashMapOf(Pair("shoes", 29.99), Pair("socks", 5.99), Pair("jeans", 39.99))
 
-    for (item in products.keys) {
-        val finalPrice = priceAfterTax(products[item])
-        println("$item cost $finalPrice")
+//    for (key in products.keys) {
+//        val finalPrice = priceAfterTax(products[key])
+//        println("$key cost $finalPrice")
+//    }
+
+    for ((key, value) in products) {
+        val finalPrice = priceAfterTax(value)
+        println("$key cost $finalPrice")
     }
 }
 
-fun priceAfterTax(price: Double?) = price?.times(1.2)
+fun priceAfterTax(price: Double) = price * 1.2
