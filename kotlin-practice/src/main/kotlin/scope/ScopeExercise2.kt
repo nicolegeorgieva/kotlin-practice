410735a172e9a8510a3549d8d5c3fceae2c9ad0f
Nicole <nicole.georgievva@gmail.com>
2023-01-25 14:17:53 +0200

    Solve scope exercise 2

---
+++ kotlin-practice/src/main/kotlin/scope/ScopeExercise2.kt
@@ -0,0 +1,11 @@
+package scope
+
+fun main() {
+    val parakeets = 5
+    buyMoreParakeets(parakeets)
+    println("You now have $parakeets parakeets.")
+}
+
+fun buyMoreParakeets(parakeets: Int) {
+    val parakeets = 3
+}
