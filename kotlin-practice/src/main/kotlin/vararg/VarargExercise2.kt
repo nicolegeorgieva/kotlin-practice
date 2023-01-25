93126a466e5fd4338ecb6fc8071fa96fb7296d7e
Nicole <nicole.georgievva@gmail.com>
2023-01-25 14:39:40 +0200

    Solve vararg exercise 2

---
+++ kotlin-practice/src/main/kotlin/vararg/VarargExercise2.kt
@@ -0,0 +1,13 @@
+package vararg
+
+fun main() {
+    sayHello(3, "Nicole", "Iliyan")
+}
+
+private fun sayHello(count: Int, vararg names: String) {
+    for (name in names) {
+        for (i in 1..count) {
+            println("Hello, $name!")
+        }
+    }
+}
