ba620775c20c88431cdba9c5396cbe784f04ee2e
Nicole <nicole.georgievva@gmail.com>
2023-01-28 18:46:40 +0200

    Solve companion object exercise

---
+++ kotlin-practice/src/main/kotlin/companionobject/Exercise.kt
@@ -0,0 +1,14 @@
+package companionobject
+
+fun main() {
+    KotlinCourse.printInfo()
+}
+
+class KotlinCourse() {
+    companion object {
+        fun printInfo() {
+            println("Kotlin is an innovative programming language.")
+            println("This course's main goal is to master the Kotlin language concepts.")
+        }
+    }
+}
