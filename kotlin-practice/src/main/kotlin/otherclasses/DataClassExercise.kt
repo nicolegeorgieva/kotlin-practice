f299505d05f75335630432bffb6dbee959921b62
Nicole <nicole.georgievva@gmail.com>
2023-02-07 18:27:29 +0200

    Solve data class exercise

---
+++ kotlin-practice/src/main/kotlin/otherclasses/DataClassExercise.kt
@@ -0,0 +1,22 @@
+package otherclasses
+
+fun main() {
+    val customers = mutableListOf<CustomerInfo>()
+
+    customers.add(CustomerInfo(name = "Iliyan", email = "ig@gmail.com", productsBought = 3))
+    customers.add(CustomerInfo(name = "Nicole", email = "ng@gmail.com", productsBought = 5))
+    customers.add(CustomerInfo(name = "Freya", email = "freya@gmail.com", productsBought = 2))
+    println(customers)
+
+    val newCustomer = customers[1].copy(email = "n.g@gmail.com")
+    customers.add(newCustomer)
+    println(customers)
+
+    sendEmail(customers)
+}
+
+fun sendEmail(customers: MutableCollection<CustomerInfo>) {
+    customers.forEach { _ -> println("Welcome email") }
+}
+
+data class CustomerInfo(val name: String, val email: String, val productsBought: Int)
