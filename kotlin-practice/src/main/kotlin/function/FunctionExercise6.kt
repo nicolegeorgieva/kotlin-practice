27a2a7b0daf6386a744c52069e0525fd271bd048
Nicole <nicole.georgievva@gmail.com>
2023-02-09 13:50:22 +0200

    Add documentation to `animalGroupLifespan` function

--- kotlin-practice/src/main/kotlin/function/FunctionExercise6.kt
+++ kotlin-practice/src/main/kotlin/function/FunctionExercise6.kt
@@ -6,11 +6,17 @@ fun main() {
     println("The estimated lifespan for that animal group is ${animalGroupLifespan(input)}.")
 }
 
+/**
+ * Returns the lifespan of different animal groups.
+ * @param animalGroup the name of the animal group _(e.g. "cats", "dogs", etc.)_
+ * @return [Int], representing the lifespan of the animal group.
+ * For unknown animal group returns 20.
+ */
 fun animalGroupLifespan(animalGroup: String): Int {
     return when (animalGroup) {
         "cats" -> 15
         "dogs" -> 10
-        "rabbit" -> 12
+        "rabbits" -> 12
         "mice" -> 2
         else -> 20
     }
