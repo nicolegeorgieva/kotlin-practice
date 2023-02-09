5bedc0f005892c23ebb67c475ec33b6569d62a44
Nicole <nicole.georgievva@gmail.com>
2023-02-09 15:12:35 +0200

    Add coroutines

--- kotlin-practice/build.gradle.kts
+++ kotlin-practice/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     testImplementation(kotlin("test"))
     testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
     testImplementation("io.kotest:kotest-assertions-core:5.5.5")
