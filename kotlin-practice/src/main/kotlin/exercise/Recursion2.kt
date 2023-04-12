1b514862b4f38ec9a71ccfad53e62abf94d951d0
Nicole <nicole.georgievva@gmail.com>
2023-04-12 13:27:52 +0300

    Exercise recursion

--- kotlin-practice/src/main/kotlin/exercise/Recursion2.kt
+++ kotlin-practice/src/main/kotlin/exercise/Recursion2.kt
@@ -1,10 +1,14 @@
 package exercise
 
 fun main() {
-    TODO()
+    println(sumN(-2))
 }
 
-// sum of integers from 1 to N
+// sum of integers from 1 to N (n = 2, 1 + 2 = 3)
 fun sumN(n: Int): Int {
-    TODO()
+    return when {
+        n <= 0 -> 0
+        n == 1 -> 1
+        else -> sumN(n - 1) + n
+    }
 }
