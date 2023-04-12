6d03550ca3333820c8118a4043b8ea6fd6b356e7
Nicole <nicole.georgievva@gmail.com>
2023-04-12 15:28:52 +0300

    Exercise Map

---
+++ kotlin-practice/src/main/kotlin/exercise/ExtensionFunctions.kt
@@ -0,0 +1,15 @@
+package exercise
+
+fun main() {
+    println(wordFrequency("the dog stays in the dog house when nobody lets it in"))
+}
+
+/*
+Input: "the dog stays in the dog house when nobody lets it in" - only lowercase letters and spaces
+Output: {the=2, dog=2, ...}
+ */
+
+fun wordFrequency(sentence: String): Map<String, Int> {
+    // Split the sentence into words and use the 'groupingBy' function to create a frequency map
+    return sentence.split(" ").groupingBy { it }.eachCount()
+}
