32e7d19770a4df98676ea193a77886abc01c5ac8
Nicole <nicole.georgievva@gmail.com>
2023-04-12 13:37:27 +0300

    Exercise recursion

--- kotlin-practice/src/main/kotlin/exercise/Recursion.kt
+++ kotlin-practice/src/main/kotlin/exercise/Recursion.kt
@@ -2,11 +2,17 @@ package exercise
 
 fun main() {
     try {
-        print(factorial(-10))
+        print(factorial(5))
     } catch (e: IllegalArgumentException) {
         println("n should be a positive number greater than 0.")
     }
 }
+
+// factorial(1) -> 1
+// factorial(2) -> factorial(2 - 1) * 2 = 1 * 2 = 2
+// factorial(3) -> factorial(3 - 1) * 3 = 2 * 3 = 6
+// factorial(4) -> factorial(4 - 1) * 4 = 6 * 4 = 24
+// factorial(5) -> factorial(5 - 1) * 5 = 24 * 5 = 120
 
 fun factorial(n: Int): Int {
     return if (n <= 0) {
--- kotlin-practice/src/main/kotlin/exercise/Recursion2.kt
+++ kotlin-practice/src/main/kotlin/exercise/Recursion2.kt
@@ -1,8 +1,14 @@
 package exercise
 
 fun main() {
-    println(sumN(-2))
+    println(sumN(5))
 }
+
+// sumN(1) = 1
+// sumN(2) = 3
+// sumN(3) = sumN(3 - 1) + 3 = sumN(2) + 3 = 3 + 3 = 6
+// sumN(4) = sumN(4 - 1) + 4 = 6 + 4 = 10
+// sumN(5) = sumN(5 - 1) + 5 = 10 + 5 = 15
 
 // sum of integers from 1 to N (n = 2, 1 + 2 = 3)
 fun sumN(n: Int): Int {
