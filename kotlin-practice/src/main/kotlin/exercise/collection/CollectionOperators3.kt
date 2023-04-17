72c8f2156826e27195fe8ee6d12b652d950a8172
Nicole <nicole.georgievva@gmail.com>
2023-04-17 13:23:35 +0300

    Exercise collection operators - .find

--- kotlin-practice/src/main/kotlin/exercise/collection/CollectionOperators3.kt
+++ kotlin-practice/src/main/kotlin/exercise/collection/CollectionOperators3.kt
@@ -1,2 +1,37 @@
 package exercise.collection
 
+fun main() {
+    val employees = listOf(
+        Employee(
+            employeeId = 124,
+            name = "Kayla",
+            department = "HR",
+            salary = 2000.00
+        ),
+        Employee(
+            employeeId = 125,
+            name = "Lulu",
+            department = "IT",
+            salary = 5600.00
+        ),
+        Employee(
+            employeeId = 126,
+            name = "Amy",
+            department = "Customer support",
+            salary = 1500.00
+        )
+    )
+
+    println(findEmployeeById(employees, 126))
+}
+
+data class Employee(
+    val employeeId: Int,
+    val name: String,
+    val department: String,
+    val salary: Double
+)
+
+fun findEmployeeById(employees: List<Employee>, id: Int): Employee? {
+    return employees.find { it.employeeId == id }
+}
