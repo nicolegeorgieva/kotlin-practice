56e79ed6d9c5b39a4bec95ce6f9a882a2a9c5072
Nicole <nicole.georgievva@gmail.com>
2023-04-18 13:41:34 +0300

    Exercise data classes and collection operators - .filter, .sortedBy

--- kotlin-practice/src/main/kotlin/exercise/dataclass/Exercise.kt
+++ kotlin-practice/src/main/kotlin/exercise/dataclass/Exercise.kt
@@ -2,12 +2,12 @@ package exercise.dataclass
 
 fun main() {
     val movies = listOf(
-        Movie(title = "Flower", releaseYear = 2005, director = "Amy Flower", rating = 9.5),
+        Movie(title = "Flower", releaseYear = 2020, director = "Amy Flower", rating = 9.5),
         Movie(title = "Parrot", releaseYear = 2003, director = "Lulu Green", rating = 5.5),
-        Movie(title = "Dog", releaseYear = 2020, director = "Amy Flower", rating = 9.0)
+        Movie(title = "Dog", releaseYear = 2005, director = "Amy Flower", rating = 9.0)
     )
 
-    println(filterMoviesByRating(movies, 8.0))
+    println(sortMoviesByYear(movies, 2005))
 }
 
 data class Movie(
@@ -17,7 +17,12 @@ data class Movie(
     val rating: Double
 )
 
-// movies with a rating above X rating
+// movies with a rating above or equal to X rating
 fun filterMoviesByRating(movies: List<Movie>, rating: Double): List<Movie> {
     return movies.filter { it.rating >= rating }
+}
+
+// movies released after or equal to Y year
+fun sortMoviesByYear(movies: List<Movie>, year: Int): List<Movie> {
+    return movies.filter { it.releaseYear >= year }.sortedBy { it.releaseYear }
 }
