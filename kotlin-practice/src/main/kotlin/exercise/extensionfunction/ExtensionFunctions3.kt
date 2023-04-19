7457ee2fbdad976f6ee36b072a18d504fac11f58
Nicole <nicole.georgievva@gmail.com>
2023-04-19 13:59:15 +0300

    Create own extension function - String.consonants()

--- kotlin-practice/src/main/kotlin/exercise/extensionfunction/ExtensionFunctions3.kt
+++ kotlin-practice/src/main/kotlin/exercise/extensionfunction/ExtensionFunctions3.kt
@@ -5,16 +5,30 @@ fun main() {
 
     println("Original String: $testString")
     println("Vowels: ${testString.vowels()}")
-//    println("Consonants: ${testString.consonants()}")
+    println("Consonants: ${testString.consonants()}")
 //    println("Capitalized Words: ${testString.capitalizeWords()}")
 }
 
 fun String.vowels(): String {
-    val vowels = listOf('a', 'e', 'i', 'o', 'u')
+    val vowels = listOf('A', 'E', 'I', 'O', 'U')
     var res = ""
 
-    for (char in this) {
+    for (char in this.uppercase()) {
         if (char in vowels) res += char
+    }
+
+    return res.toSet().joinToString(", ")
+}
+
+fun String.consonants(): String {
+    val consonants = listOf(
+        'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
+    )
+
+    var res = ""
+
+    for (char in this.uppercase()) {
+        if (char in consonants) res += char
     }
 
     return res.toSet().joinToString(", ")
