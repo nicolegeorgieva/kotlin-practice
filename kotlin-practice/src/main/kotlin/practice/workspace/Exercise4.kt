0fe2f0c8a13c09fe0f684ce6da82da3d6ad0f730
Nicole <nicole.georgievva@gmail.com>
2023-05-11 13:51:42 +0300

    Exercise repeat()

--- kotlin-practice/src/main/kotlin/practice/workspace/Exercise4.kt
+++ kotlin-practice/src/main/kotlin/practice/workspace/Exercise4.kt
@@ -1,6 +1,13 @@
 package practice.workspace
 
 fun main() {
+    val treatFunction = trickOrTreat(false) { "$it quarters" }
+    val trickFunction = trickOrTreat(true, null)
 
+    repeat(4) {
+        treatFunction()
+    }
+
+    trickFunction()
 }
 
