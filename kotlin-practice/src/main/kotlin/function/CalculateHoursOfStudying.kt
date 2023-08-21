3a46bc308d0d1b0f904af0b0793bfbfcee53bd63
Nicole <nicole.georgievva@gmail.com>
2023-08-21 20:34:01 +0300

    Add documentation to CalculateHoursOfStudying exercise's functions

--- kotlin-practice/src/main/kotlin/function/CalculateHoursOfStudying.kt
+++ kotlin-practice/src/main/kotlin/function/CalculateHoursOfStudying.kt
@@ -3,7 +3,7 @@ package function
 fun main() {
     var calculatedTime = 3.0
 
-    val currentStudyMins = calculateCurrentTimeOfStudyingInMin("19:00", "20:16")
+    val currentStudyMins = calculateCurrentTimeOfStudyingInMin("19:00", "20:33")
     val currentStudy = calculateCurrentTimeOfStudying(currentStudyMins, cutMins = 4)
 
     calculatedTime = calculateTotalTimeOfStudying(calculatedTime, currentStudy)
@@ -11,10 +11,18 @@ fun main() {
     println(calculatedTime)
 }
 
+/**
+ * This fun receives the already calculated time of studying (e.g. 1.2) and current time of studying (e.g. 2.2) and
+ * returns a Double representing the sum of both values (3.2 in this example, which means 3h 20m).
+ */
 fun calculateTotalTimeOfStudying(calculatedTime: Double, currentTime: Double): Double {
     return calculatedTime + currentTime
 }
 
+/**
+ * This fun receives currentMins and cutMins* and returns a Double value representing the currentTimeOfStudying
+ * (e.g. 1.2) in which "1" means 1 hour and "2" means 20 minutes. Or total - 1h 20m.
+ */
 fun calculateCurrentTimeOfStudying(mins: Int, cutMins: Int = 0): Double {
     var hour = ""
     var min = ""
@@ -33,6 +41,10 @@ fun calculateCurrentTimeOfStudying(mins: Int, cutMins: Int = 0): Double {
 }
 
 // 15:20 - 16:40 (80m)
+/**
+ * This fun receives startTime (e.g. 15:20) and endTime (e.g. 16:40) and calculates current time of studying in mins
+ * (80 for this example).
+ */
 fun calculateCurrentTimeOfStudyingInMin(startTime: String, endTime: String): Int {
     val minutesPerHour = 60
 
