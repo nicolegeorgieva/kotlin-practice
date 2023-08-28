20dec776b6c13b7918494884d5dd9f6300488070
Nicole <nicole.georgievva@gmail.com>
2023-08-28 15:05:10 +0300

    WIP: GenericsExercise

--- kotlin-practice/src/main/kotlin/book/GenericsExercise.kt
+++ kotlin-practice/src/main/kotlin/book/GenericsExercise.kt
@@ -1,11 +1,16 @@
 package book
 
 fun main() {
+    val catContest = Contest<Cat>()
 
+    catContest.addScore(Cat("Fuzz"), 50)
+    catContest.addScore(Cat("Katsu"), 45)
+
+    val topCat = catContest.getWinners().first().name
+    println(topCat)
 }
 
 abstract class Pet(var name: String)
-
 class Cat(name: String) : Pet(name)
 class Dog(name: String) : Pet(name)
 class Fish(name: String) : Pet(name)
@@ -15,5 +20,16 @@ class Contest<T : Pet> {
 
     fun addScore(t: T, score: Int = 0) {
         if (score >= 0) scores.put(t, score)
+    }
+
+    fun getWinners(): MutableSet<T> {
+        val highScore = scores.values.max()
+        val winners: MutableSet<T> = mutableSetOf()
+
+        for ((t, score) in scores) {
+            if (score == highScore) winners.add(t)
+        }
+
+        return winners
     }
 }
