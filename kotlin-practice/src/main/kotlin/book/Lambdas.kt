3988b7014bbd6b6b1f5bce9a7da5250c5e7daca0
Nicole <nicole.georgievva@gmail.com>
2023-08-30 16:37:07 +0300

    WIP: Lambdas exercise

--- kotlin-practice/src/main/kotlin/book/Lambdas.kt
+++ kotlin-practice/src/main/kotlin/book/Lambdas.kt
@@ -3,4 +3,10 @@ package book
 fun main() {
     var lambda4: (Double) -> Unit = { println("Hi") }
     lambda4(5.0)
+
+    println(convert(2.5) { it * 2 })
+}
+
+fun convert(number: Double, converter: (Double) -> Double): Double {
+    return converter(number)
 }
