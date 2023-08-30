abdfd0b28bdbb795ce925cd518cb554718ce61d6
Nicole <nicole.georgievva@gmail.com>
2023-08-30 22:05:38 +0300

    Improve Lambdas 3 exercise

--- kotlin-practice/src/main/kotlin/book/Lambdas3.kt
+++ kotlin-practice/src/main/kotlin/book/Lambdas3.kt
@@ -5,16 +5,36 @@ fun main() {
     println(lambda)
 
     println(convert(2.5, getConversionLambda("KgsToPounds")))
+
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+
+    val kgToTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgToTonsLambda)} US tons.")
 }
 
 fun getConversionLambda(str: String): (Double) -> Double {
-    if (str == "CentigradeToFahrenheit") {
-        return { it * 1.8 + 32 }
-    } else if (str == "KgsToPounds") {
-        return { it * 2.204623 }
-    } else if (str == "PoundsToUSTons") {
-        return { it / 2000.0 }
-    } else {
-        return { it }
+    return when (str) {
+        "CentigradeToFahrenheit" -> {
+            { it * 1.8 + 32 }
+        }
+
+        "KgsToPounds" -> {
+            { it * 2.204623 }
+        }
+
+        "PoundsToUSTons" -> {
+            { it / 2000.0 }
+        }
+
+        else -> {
+            { it }
+        }
     }
+}
+
+fun combine(lambda1: (Double) -> Double, lambda2: (Double) -> Double): (Double) -> Double {
+    return { x: Double -> lambda2(lambda1(x)) }
 }
