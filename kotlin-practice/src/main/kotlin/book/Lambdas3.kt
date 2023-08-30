c8272ced8e249676fff78893bfa23f8db98b963d
Nicole <nicole.georgievva@gmail.com>
2023-08-30 21:40:32 +0300

    Solve Lambdas 3 exercise

---
+++ kotlin-practice/src/main/kotlin/book/Lambdas3.kt
@@ -0,0 +1,20 @@
+package book
+
+fun main() {
+    val pounds = getConversionLambda("KgsToPounds")(2.5)
+    println(pounds)
+
+    println(convert(2.5, getConversionLambda("KgsToPounds")))
+}
+
+fun getConversionLambda(str: String): (Double) -> Double {
+    if (str == "CentigradeToFahrenheit") {
+        return { it * 1.8 + 32 }
+    } else if (str == "KgsToPounds") {
+        return { it * 2.204623 }
+    } else if (str == "PoundsToUSTons") {
+        return { it / 2000.0 }
+    } else {
+        return { it }
+    }
+}
