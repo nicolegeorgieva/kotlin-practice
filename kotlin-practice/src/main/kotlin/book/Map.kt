9c169fac40eb23d263823d9753c444da0b2c54c1
Nicole <nicole.georgievva@gmail.com>
2023-08-31 14:14:30 +0300

    Solve .map exercise

--- kotlin-practice/src/main/kotlin/book/Map.kt
+++ kotlin-practice/src/main/kotlin/book/Map.kt
@@ -5,4 +5,15 @@ fun main() {
 
     println(myMap.values.sum())
     println(myMap.keys.size)
+
+
+    val groceries2 = listOf(
+        Grocery("Tomatoes", "Vegetables", "lb", 5.2),
+        Grocery(
+            "Mushrooms", "Vegetables", "lb", 4.0
+        )
+    )
+
+    val groceryNames = groceries2.map { it.name }
+    println(groceryNames)
 }
