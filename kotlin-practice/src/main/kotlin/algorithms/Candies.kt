b3a9124c68304902298c1047e615b8aacc64cf2d
Nicole <nicole.georgievva@gmail.com>
2023-11-15 16:07:23 +0200

    Solve Candies exercise

---
+++ kotlin-practice/src/main/kotlin/algorithms/Candies.kt
@@ -0,0 +1,22 @@
+package algorithms
+
+fun main() {
+    val candies = intArrayOf(2, 3, 5, 1, 3)
+    val extraCandies = 3
+
+    println(kidsWithCandies(candies, extraCandies))
+}
+
+private fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
+    val res = mutableListOf<Boolean>()
+
+    val maxKid = candies.maxBy { it }
+
+    for (kid in candies) {
+        val currentKid = kid + extraCandies
+
+        res.add(currentKid >= maxKid)
+    }
+
+    return res
+}
