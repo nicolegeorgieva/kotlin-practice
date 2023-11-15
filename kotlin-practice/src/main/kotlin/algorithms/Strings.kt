c54e2049e6cf4195e2da7724ea02c3c7c4657065
Nicole <nicole.georgievva@gmail.com>
2023-11-15 15:29:53 +0200

    Solve more optimally and shortly strings exercise

--- kotlin-practice/src/main/kotlin/algorithms/Strings.kt
+++ kotlin-practice/src/main/kotlin/algorithms/Strings.kt
@@ -1,7 +1,5 @@
 package algorithms
 
-import kotlin.math.abs
-
 fun main() {
     val word1 = "cdf"
     val word2 = "a"
@@ -10,32 +8,16 @@ fun main() {
 }
 
 private fun mergeAlternately(word1: String, word2: String): String {
-    var res = ""
-    var endLetters = ""
-    val difference = word1.length - word2.length
+    val longerLength = maxOf(word1.length, word2.length)
 
-    if (word1.length > word2.length) {
-        val newWord1 = word1.dropLast(difference)
-        endLetters = word1.drop(newWord1.length)
-
-        for (i in newWord1.indices) {
-            res += newWord1[i]
-            res += word2[i]
-        }
-    } else if (word2.length > word1.length) {
-        val newWord2 = word2.dropLast(abs(difference))
-        endLetters = word2.drop(newWord2.length)
-
-        for (i in newWord2.indices) {
-            res += word1[i]
-            res += newWord2[i]
-        }
-    } else {
-        for (i in word1.indices) {
-            res += word1[i]
-            res += word2[i]
+    return buildString {
+        for (i in 0 until longerLength) {
+            word1.getOrNull(i)?.let {
+                append(it)
+            }
+            word2.getOrNull(i)?.let {
+                append(it)
+            }
         }
     }
-
-    return res + endLetters
 }
