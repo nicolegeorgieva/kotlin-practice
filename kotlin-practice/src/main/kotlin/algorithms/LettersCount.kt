57307003cd4fbe641f249b92413ab341a93a7e5f
Nicole <nicole.georgievva@gmail.com>
2023-11-20 18:25:49 +0200

    Solve LettersCount exercise more optimally

--- kotlin-practice/src/main/kotlin/algorithms/LettersCount.kt
+++ kotlin-practice/src/main/kotlin/algorithms/LettersCount.kt
@@ -8,19 +8,19 @@ fun main() {
 
 // aaabcb -> Map(Pair(a, 3), Pair(b, 2), Pair(c, 1))
 private fun String.countLetters(): Map<Char, Int> {
-    val res = mutableMapOf<Char, Int>()
+    val charsCountMap = mutableMapOf<Char, Int>()
 
     for (i in this.indices) {
         val char = this[i]
 
-        if (res.contains(char)) {
-            val currentCount = res[char] ?: 1
-            res[char] = currentCount + 1
+        if (charsCountMap.contains(char)) {
+            val currentCount = charsCountMap[char] ?: 1
+            charsCountMap[char] = currentCount + 1
             continue
         }
 
-        res[char] = 1
+        charsCountMap[char] = 1
     }
 
-    return res
+    return charsCountMap
 }
