6e6beb318a5adae652088ae2a54099b531c44241
Nicole <nicole.georgievva@gmail.com>
2023-11-21 18:26:37 +0200

    Solve Zeroes exercise

---
+++ kotlin-practice/src/main/kotlin/algorithms/Zeroes.kt
@@ -0,0 +1,33 @@
+package algorithms
+
+fun main() {
+    val nums = intArrayOf(0, 1, 2, 3, 0)
+
+    moveZeroes(nums)
+
+    println(nums.toList())
+}
+
+/*
+Input: nums = [0,1,0,3,12]
+Input: nums = [1,3,12,0,0]
+Output: [1,3,12,0,0]
+
+Input: nums = [0]
+Output: [0]
+ */
+private fun moveZeroes(nums: IntArray) {
+    val new = nums.filter {
+        it != 0
+    }.toMutableList()
+
+    val zeroesToAdd = nums.size - new.size
+
+    for (i in 1..zeroesToAdd) {
+        new += 0
+    }
+
+    for (i in new.indices) {
+        nums[i] = new[i]
+    }
+}
