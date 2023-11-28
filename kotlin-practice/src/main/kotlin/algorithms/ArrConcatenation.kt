73e2a4a79c5b4bb5d906d22b12bed815c4575444
Nicole <nicole.georgievva@gmail.com>
2023-11-28 17:39:41 +0200

    Solve ArrConcatenation exercise

---
+++ kotlin-practice/src/main/kotlin/algorithms/ArrConcatenation.kt
@@ -0,0 +1,14 @@
+package algorithms
+
+fun main() {
+    val arr = intArrayOf(1, 2, 1)
+    println(getConcatenation(arr).toList())
+}
+
+/*
+Input: nums = [1,2,1] -> 2x
+Output: [1,2,1,1,2,1]
+ */
+private fun getConcatenation(nums: IntArray): IntArray {
+    return nums + nums
+}
