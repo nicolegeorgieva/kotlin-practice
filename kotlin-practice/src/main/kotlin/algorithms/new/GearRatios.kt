a845f6d0701ee6c768c4ef20cd142b56214a6453
Nicole <nicole.georgievva@gmail.com>
2023-12-05 13:14:23 +0200

    WIP: Solve GearRatios exercise - implement isANumber function

--- kotlin-practice/src/main/kotlin/algorithms/new/GearRatios.kt
+++ kotlin-practice/src/main/kotlin/algorithms/new/GearRatios.kt
@@ -4,12 +4,21 @@ import java.io.File
 
 fun main() {
     val input = File("gearRatios.txt").readText()
-
-    println(isASymbol("%"))
 }
 
-private fun isASymbol(input: String): Boolean {
-    if (input == ".") return false
-    if (input.toIntOrNull() != null) return false
+private fun isASymbol(input: Char): Boolean {
+    if (input == '.') return false
+    if (input.isDigit()) return false
     return true
 }
+
+private fun isANumber(input: Char): Boolean {
+    return input.isDigit()
+}
+
+/*
+to match criteria for part number:
+1. A number which is directly next to a symbol (left || right) ||
+2. A number which is next || same index to a symbol (previous || same || next)
+ */
+
