8b170180828fcb6ddceeeec6a830bf11eb0eb87e
Nicole <nicole.georgievva@gmail.com>
2023-12-05 13:38:55 +0200

    WIP: Solve GearRatios exercise

--- kotlin-practice/src/main/kotlin/algorithms/new/GearRatios.kt
+++ kotlin-practice/src/main/kotlin/algorithms/new/GearRatios.kt
@@ -6,19 +6,46 @@ fun main() {
     val input = File("gearRatios.txt").readText()
 }
 
-private fun isASymbol(input: Char): Boolean {
-    if (input == '.') return false
-    if (input.isDigit()) return false
-    return true
-}
-
-private fun isANumber(input: Char): Boolean {
-    return input.isDigit()
-}
-
 /*
 to match criteria for part number:
 1. A number which is directly next to a symbol (left || right) ||
 2. A number which is next || same index to a symbol (previous || same || next)
  */
 
+// "617*......"
+// A number which is directly next to a symbol (left || right)
+private fun partNumbersFromDirectlyNextSymbols(input: String): List<Int> {
+    var latestChar = ' '
+    var number = ""
+    val partNumbersList = mutableListOf<Int>()
+
+    for (i in input.indices) {
+        latestChar = input[i]
+
+        if (latestChar.isANumber()) {
+            number += latestChar
+        } else if (latestChar.isASymbol()) {
+            if (input[i - 1].isANumber()) {
+                partNumbersList += number.toInt()
+            }
+
+            if (input[i + 1].isANumber()) {
+                number = ""
+            }
+        } else {
+            continue
+        }
+    }
+
+    return partNumbersList
+}
+
+private fun Char.isASymbol(): Boolean {
+    if (this == '.') return false
+    if (this.isDigit()) return false
+    return true
+}
+
+private fun Char.isANumber(): Boolean {
+    return this.isDigit()
+}
